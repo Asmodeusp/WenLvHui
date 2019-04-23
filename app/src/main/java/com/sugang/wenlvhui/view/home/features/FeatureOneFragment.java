a0ef458af2c15f95068c17a9eb923cd5721e13c0
Asmodeusp <762063243@qq.com>
2019-04-23 10:50:55 +0800

    添加頁面之間跳轉

--- app/src/main/java/com/sugang/wenlvhui/view/home/features/FeatureOneFragment.java
+++ app/src/main/java/com/sugang/wenlvhui/view/home/features/FeatureOneFragment.java
@@ -1,19 +1,49 @@
 package com.sugang.wenlvhui.view.home.features;
 
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.sugang.wenlvhui.R;
 import com.sugang.wenlvhui.base.BaseFragment;
+import com.sugang.wenlvhui.view.home.intangiblecultural.IntangibleCulturalHeritageActivity;
+import com.sugang.wenlvhui.view.home.literarytraining.LiteraryTrainingActivity;
+import com.sugang.wenlvhui.view.home.localcuisine.LocalcuisineActivity;
+import com.sugang.wenlvhui.view.home.travelroute.TravelRouteActivity;
+import com.zhy.autolayout.AutoLinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FeatureOneFragment extends BaseFragment {
 
+    //文艺培训
+    @BindView(R.id.Featrue_LiteraryTrainingButton)
+    AutoLinearLayout FeatrueLiteraryTrainingButton;
+    //非遗文创
+    @BindView(R.id.Featrue_IntangibleCulturalHeritageButton)
+    AutoLinearLayout FeatrueIntangibleCulturalHeritageButton;
+    //地方美食
+    @BindView(R.id.Featrue_LocalcuisineButton)
+    AutoLinearLayout FeatrueLocalcuisineButton;
+    //文旅路线
+    @BindView(R.id.Featrue_TravelRouteButton)
+    AutoLinearLayout FeatrueTravelRouteButton;
+
 
     public FeatureOneFragment() {
         // Required empty public constructor
     }
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_feature_one;
@@ -29,4 +59,26 @@ public class FeatureOneFragment extends BaseFragment {
 
     }
 
+
+    @OnClick({R.id.Featrue_LiteraryTrainingButton, R.id.Featrue_IntangibleCulturalHeritageButton, R.id.Featrue_LocalcuisineButton, R.id.Featrue_TravelRouteButton})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            //文艺培训
+            case R.id.Featrue_LiteraryTrainingButton:
+                this.startActivity(new Intent(getActivity(), LiteraryTrainingActivity.class));
+                break;
+            //非遗文创
+            case R.id.Featrue_IntangibleCulturalHeritageButton:
+                this.startActivity(new Intent(getActivity(),IntangibleCulturalHeritageActivity.class));
+                break;
+            //地方美食
+            case R.id.Featrue_LocalcuisineButton:
+                this.startActivity(new Intent(getActivity(), LocalcuisineActivity.class));
+                break;
+            case R.id.Featrue_TravelRouteButton:
+                //文旅路线
+                this.startActivity(new Intent(getActivity(), TravelRouteActivity.class));
+                break;
+        }
+    }
 }
--- app/src/main/java/com/sugang/wenlvhui/view/home/features/FeatureTwoFragment.java
+++ app/src/main/java/com/sugang/wenlvhui/view/home/features/FeatureTwoFragment.java
@@ -1,6 +1,7 @@
 package com.sugang.wenlvhui.view.home.features;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,11 +10,35 @@ import android.view.ViewGroup;
 
 import com.sugang.wenlvhui.R;
 import com.sugang.wenlvhui.base.BaseFragment;
+import com.sugang.wenlvhui.view.home.culturaltravel.CulturalTravelPolicyActivity;
+import com.sugang.wenlvhui.view.home.culturaltravelcustomization.CulturalTravelCustomizationActivity;
+import com.sugang.wenlvhui.view.home.literarybrigadelist.LiteraryBrigadeListActivity;
+import com.sugang.wenlvhui.view.home.literarytraining.LiteraryTrainingActivity;
+import com.sugang.wenlvhui.view.home.prefebook.RecommendedBooksActivity;
+import com.zhy.autolayout.AutoLinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FeatureTwoFragment extends BaseFragment {
+
+    //文旅榜单
+    @BindView(R.id.Featrue_LiteraryBrigadeListButton)
+    AutoLinearLayout FeatrueLiteraryBrigadeListButton;
+    //文旅定制
+    @BindView(R.id.Featrue_CulturalTravelCustomizationButton)
+    AutoLinearLayout FeatrueCulturalTravelCustomizationButton;
+    //文旅政策
+    @BindView(R.id.Featrue_CulturalTravelPolicyButton)
+    AutoLinearLayout FeatrueCulturalTravelPolicyButton;
+    //好书推荐
+    @BindView(R.id.Featrue_RecommendedBooksButton)
+    AutoLinearLayout FeatrueRecommendedBooksButton;
 
 
     public FeatureTwoFragment() {
@@ -35,4 +60,26 @@ public class FeatureTwoFragment extends BaseFragment {
 
     }
 
+
+    @OnClick({R.id.Featrue_LiteraryBrigadeListButton, R.id.Featrue_CulturalTravelCustomizationButton, R.id.Featrue_CulturalTravelPolicyButton, R.id.Featrue_RecommendedBooksButton})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            //文旅榜单
+            case R.id.Featrue_LiteraryBrigadeListButton:
+                this.startActivity(new Intent(getActivity(), LiteraryBrigadeListActivity.class));
+                break;
+            //文旅定制
+            case R.id.Featrue_CulturalTravelCustomizationButton:
+                this.startActivity(new Intent(getActivity(), CulturalTravelCustomizationActivity.class));
+                break;
+            //文旅政策
+            case R.id.Featrue_CulturalTravelPolicyButton:
+                this.startActivity(new Intent(getActivity(), CulturalTravelPolicyActivity.class));
+                break;
+            //好书推荐
+            case R.id.Featrue_RecommendedBooksButton:
+                this.startActivity(new Intent(getActivity(), RecommendedBooksActivity.class));
+                break;
+        }
+    }
 }
